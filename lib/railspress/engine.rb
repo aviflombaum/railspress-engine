735dad43d4f9dcafd625e16253873dee52ed7452
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2025-12-24 10:07:06 -0600

    "refactored css"

--- lib/railspress/engine.rb
+++ lib/railspress/engine.rb
@@ -6,6 +6,7 @@ module Railspress
     initializer "railspress.assets" do |app|
       if app.config.respond_to?(:assets)
         app.config.assets.paths << root.join("app", "assets", "stylesheets").to_s
+        app.config.assets.paths << root.join("app", "assets", "stylesheets", "railspress").to_s
         app.config.assets.paths << root.join("app", "assets", "javascripts").to_s
         app.config.assets.paths << root.join("app", "assets", "images").to_s
       end
